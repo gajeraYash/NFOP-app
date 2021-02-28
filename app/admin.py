2c1f6587fca3d5d913f8733e5fe8a23dd150f073
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2021-02-28 00:44:37 -0500

    UserProfile + UserStatus Model ⏫

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from app.models import *
 
+# Modify your admin layout here.
+class UserStatusAdmin(admin.ModelAdmin):
+    search_fields = ['user__first_name', 'user__last_name', 'user__username']
+    list_filter = ['status','user__username','user__first_name', 'user__last_name' ]
+    list_display = ['user', 'get_user_firstname', 'get_user_lastname','status' ]
+    list_editable = ['status']
+
+    def get_user_firstname(self, obj):
+        return obj.user.first_name
+    get_user_firstname.admin_order_field  = 'user__first_name'
+    get_user_firstname.short_description = 'First name'
+
+    def get_user_lastname(self, obj):
+        return obj.user.last_name
+    get_user_lastname.admin_order_field  = 'user__last_name'
+    get_user_lastname.short_description = 'Last name'
+
+class UserProfileAdmin(admin.ModelAdmin):
+    search_fields = ['user__first_name', 'user__last_name', 'user__username', 'phone_number' 'address_1', 'city', 'state', 'zip_code']
+    list_filter = ['user__username','user__first_name', 'user__last_name', 'city','zip_code']
+    list_display = ['user', 'get_user_firstname', 'get_user_lastname', 'address_1', 'address_2', 'city','state','zip_code','phone_number']
+
+    def get_user_firstname(self, obj):
+        return obj.user.first_name
+    get_user_firstname.admin_order_field  = 'user__first_name'
+    get_user_firstname.short_description = 'First name'
+
+    def get_user_lastname(self, obj):
+        return obj.user.last_name
+    get_user_lastname.admin_order_field  = 'user__last_name'
+    get_user_lastname.short_description = 'Last name'
 # Register your models here.
+admin.site.register(UserProfile,UserProfileAdmin)
+admin.site.register(UserStatus,UserStatusAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class UserProfile (models.Model):
+    user = models.OneToOneField(User, default="", on_delete=CASCADE)
+    address_1 = models.CharField(max_length=128)
+    address_2 = models.CharField(max_length=128, blank=True)
+
+    city = models.CharField(max_length=64)
+    state = models.CharField(max_length=24)
+    zip_code = models.CharField(max_length=5)
+
+    phone_number = models.CharField(max_length=10)
+
+    def __str__(self):
+        return ( '('+self.user.username + ')\n' 
+        + self.user.first_name + ' '  + self.user.last_name 
+        + '\nassociated address:\n' 
+        + self.address_1 + '\n'
+        + self.address_2 + '\n'
+        + self.city + ', ' + self.state + ' ' + self.zip_code)
+    class Meta:
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'User Profiles'
+
+class UserStatus(models.Model):
+    UNVERIFIED = 'UVER'
+    VERIFIED = 'VERI'
+    ACTIVE = 'ACTI'
+    POLICE = 'POLI'
+
+    STATUS_CHOICES = [
+        (UNVERIFIED, 'Unverified'),
+        (VERIFIED, 'Verified'),
+        (ACTIVE, 'Active'),
+        (POLICE, 'Police'),
+    ]
+        
+    user = models.OneToOneField(User, on_delete=CASCADE)
+    status = models.CharField(choices=STATUS_CHOICES, default=UNVERIFIED, max_length=4)
+
+    def __str__(self):
+        return self.user.username + ' is a ' +self.status + ' status.'
+
+    def is_authorized(self):
+        return self.status in {self.POLICE, self.ACTIVE} 
+    
+    class Meta:
+        verbose_name = 'User Status'
+        verbose_name_plural = 'User Status'
