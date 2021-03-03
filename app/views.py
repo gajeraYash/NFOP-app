c1462044bedcf50a676a973e05a7b93c1456ad40
TansiU <tfu3@scarletmail.rutgers.edu>
2021-03-02 21:29:39 -0500

    Fade in effect added, minor tweaks to signup/login pages

--- app/views.py
+++ app/views.py
@@ -39,7 +39,7 @@ def signup(request):
                                                 'profile_form': profile_form,})
 
 def login(request):
-    return render(request, "app/index.html")
+    return render(request, "app/login.html")
 
 def logout(request):
     return render(request, "app/index.html")
