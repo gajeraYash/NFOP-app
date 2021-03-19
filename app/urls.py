1bb19b50b3a3e6a1d73a9fda8a0d8894c5a1cb5e
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2021-03-18 22:31:30 -0400

    Git Project Reset 📍

---
+++ app/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'app'  # here for namespacing of urls.
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('test/', views.test, name="test"),
+    
+    #About Page
+    path('about', views.about, name="about"),
+    
+    #Contact Us Page
+    path('contact', views.contact, name="contact"),
+
+    #Register Page
+    path('signup', views.signup, name="signup"),
+
+    #Login/Logout Page
+    path('login', views.login, name="login"),
+    path('logout', views.logout, name="logout"),
+
+    #Social Media Page
+    path('social_media', views.social_media, name="social_media"),
+
+    #Donate Page
+    path('donate', views.donate, name="donate"),
+
+    #temp
+    path('member', views.member, name="member"),
+
+]
+
